8fd80579770c3c58fb44088cb11b777c7efc99e2
Sonu Prajapati <sonuprajapati83497@gmail.com>
2026-01-15 08:16:46 +0530

    Update Files

---
+++ DataTypes & Variables/Relational.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Relational {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to driving Licence Portal :");
+        System.out.print("Please Enter Your age : ");
+        int age = input.nextInt();
+
+        if (age >= 18) {
+            System.out.println("You are eligible to drive !");
+        } else {
+            System.out.println("You are not fit for drive.");
+        }
+        input.close();
+    }
+}
---
+++ DataTypes & Variables/genderCheck.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class GenderCheck {
+
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("Check Gender...\n");
+            System.out.print("Please Enter Gender : ");
+            String name = input.next();
+            
+            if (name.equalsIgnoreCase("Male") || name.equalsIgnoreCase("m")) {
+                System.out.println("You are a Male. Congratulations!");
+            }
+            else if (name.equalsIgnoreCase("Female") || name.equalsIgnoreCase("f")) {
+                System.out.println("You are a Female. Congratulations!");
+            }
+            else {
+                System.out.println("Invalid gender input!");
+            }
+        }
+    }
+}
