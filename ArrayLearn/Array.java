d6a0cfff34561eec56a9f59ba9310292e87f12d5
Sonu Prajapati <sonuprajapati83497@gmail.com>
2026-02-01 10:21:55 +0530

    update file

---
+++ ArrayLearn/Array.java
@@ -0,0 +1,15 @@
+public class Array {
+
+    public static void main(String[] args) {
+        int[] myArr = new int[5];
+        myArr[0] = 95;
+        myArr[1] = 94;
+        myArr[2] = 99;
+        myArr[3] = 905;
+        myArr[4] = 9;
+
+        System.out.println(myArr[0]);
+
+    }
+
+}
